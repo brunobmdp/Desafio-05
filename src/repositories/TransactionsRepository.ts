6b9ce0d057cd51e31a1bb21731d525be78f2f589
Bruno Marques de Paula <brunobmdp@pop-os.localdomain>
2020-04-25 17:58:31 -0300

    i/o from a bancary transaction using nodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,23 +7,52 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance = this.transactions.reduce(
+      (accBalance, transaction) => {
+        if (transaction.type === 'income') {
+          accBalance.income += transaction.value;
+          accBalance.total += transaction.value;
+        } else {
+          accBalance.outcome += transaction.value;
+          accBalance.total -= transaction.value;
+        }
+        return accBalance;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
